71ffa3fda925f7043977e85225574805a79dec04
krtcotmo2 <krtcotmo2@gmail.com>
2019-04-13 14:48:36 -0400

    fix routing error

--- server.js
+++ server.js
@@ -10,15 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Use Routes folder
+app.use(express.static("client/build"));
 app.use(routes);
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/count-stockula");
 
