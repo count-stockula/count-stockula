7b51688017287297f880c1f4c62bc174d93938a8
Jarret Yingling <44506154+JarretYingling@users.noreply.github.com>
2019-04-19 02:00:59 -0400

    signup page

--- client/src/pages/SignUp/SignUp.js
+++ client/src/pages/SignUp/SignUp.js
@@ -4,6 +4,7 @@ import PageHeader from "../../components/Pageheader/Pageheader";
 import BottomBar from "../../components/BottomBar/BottomBar";
 import Form from "../../components/Form/Form";
 import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
 
 export default class SignUp extends PureComponent {
   state = {};
@@ -14,12 +15,13 @@ export default class SignUp extends PureComponent {
         <PageHeader title="Login" />
           <div className="container px-0 w-100 pb-5">
             <div className="mx-auto col-12 col-lg-8 col-md-8 col-sm-10 col-xl-7 px-0">
-              <Form>
-                <>
-                  <input type="text" class="form-control text-center" id="3" name="3" value="3" readonly="readonly" />
-                  <input type="text" class="form-control text-center" id="3" name="3" value="3" readonly="readonly" />
-                  <input type="text" class="form-control text-center" id="3" name="3" value="3" readonly="readonly" />
-                </>
+              <Form id="login" method="POST" action="/Scan">
+                  <Input type="email" className="form-control text-center" id="email" name="email" value="" placeholder="email@domain.com" />
+                  <Input type="password" className="form-control text-center" id="password" name="password" value="" placeholder="password" />
+                  <Input type="text" className="form-control text-center" id="3" name="3" value="3" placeholder="" readonly="readonly" />
+                  <Input type="text" className="form-control text-center" id="4" name="4" value="4" placeholder="" readonly="readonly" />
+                  <Input type="text" className="form-control text-center" id="5" name="5" value="5" placeholder="" readonly="readonly" />
+                  <Button type="submit">Login</Button>
               </Form>
             </div>
           </div>
