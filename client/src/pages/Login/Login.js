01a8da48dc3fe87e81dcc27ed36073855fa58e04
Jarret Yingling <44506154+JarretYingling@users.noreply.github.com>
2019-04-19 21:07:03 -0400

    login oninput to state

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -6,23 +6,44 @@ import Button from "../../components/Button/Button";
 
 export default class Login extends PureComponent {
   state = {
-    
+    email: "",
+    userPass: ""
+  };
+  
+  handleInput = (inputName) => {
+
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event);
+    API.checkPass(this.state.email, this.state.userPass)
+    .then(serverResponse => {
+      this.setState({email: "", userPass: ""});
+        // handle response from server
+        if (serverResponse === "badEmail") {
+          // handle bad email
+          return;
+        } else if (serverResponse === "badPass") {
+          // handle bad password
+          return;
+        }
+        window.location("/Scan");
+    });
   };
 
   render() {
     return (
-      <>
-        <div className="container px-0 w-100 pb-5">
-          <div className="mx-auto col-12 col-lg-8 col-md-8 col-sm-10 col-xl-7 px-0">
-            <Form id="login" method="POST" action="/Login">
-                <Input type="email" className="text-center" id="email" name="email" value="" placeholder="email@domain.com" />
-                <Input type="password" className="text-center" id="userPass" name="userPass" value="" placeholder="password" />
-                <Button type="submit">Login</Button>
-            </Form>
-            <a href="/Scan"><Button>Sign Up</Button></a>
-          </div>
+      <div className="container px-0 w-100 pb-5">
+        <div className="mx-auto col-12 col-lg-8 col-md-8 col-sm-10 col-xl-7 px-0">
+          <Form id="login">
+            <Input type="email" className="text-center" id="email" ref="" name="email" value="" placeholder="email@domain.com" oninput={this.handleInput} />
+            <Input type="password" className="text-center" id="userPass" ref="" name="userPass" value="" placeholder="password" oninput={this.handleInput} />
+            <Button onClick={this.handleSubmit}>Login</Button>
+          </Form>
+          <a href="/SignUp"><Button>Sign Up</Button></a>
         </div>
-      </>
+      </div>
     );
-  }
+  };
 }
