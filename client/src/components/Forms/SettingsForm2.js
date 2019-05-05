29b82f8d1d19c86d24fd43e0b40a094d49e35899
krtcotmo2 <krtcotmo2@gmail.com>
2019-05-04 21:31:34 -0400

    rehooked setting page

--- client/src/components/Forms/SettingsForm2.js
+++ client/src/components/Forms/SettingsForm2.js
@@ -6,7 +6,6 @@ import "./Forms.css";
 
 export default function SettingsForm(props) {
 let initialStore=props.storeId;
-console.log(initialStore)
   return (
     <Form id="aForm">
       <div className="col s12 m6">
