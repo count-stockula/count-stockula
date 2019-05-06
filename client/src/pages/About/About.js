263bf438be85bf0e8da8a4415bdb7c1e8e5a4cee
annasylvester <annaleesylvester@gmail.com>
2019-05-06 02:09:47 -0400

    redo

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -24,23 +24,30 @@ export default class About extends PureComponent {
               <br></br>
             </div>
           </div>
-        
-          {/* Instructions section */}
-          <div className="container grayFiller instructionsContainer">
-            <div className="row">
-              <div className="valign-wrapper">
-                <h5>This should be vertically aligned</h5>
+
+          {/* Parallax */}
+          <div className="container header-container">
+              <div className="section">
+                <div className="row">
+                  <div className="col s12">
+                    <img className="headliner-image" src="/images/headliner.png"></img>
+                  </div>
+                </div>
               </div>
-             </div>
           </div>
 
             {/* Icon Section */}
             <div className="container">
               <div className="section">
                 <div className="row">
+                  <div className="col s12">
+                    <h5 className="center">How it works</h5>
+                  </div>
+                </div>
+                <div className="row">
                   <div className="col s12 m4">
                     <div className="icon-block">
-                      <h2 className="center"><img src="/images/iconCoffin.png" className="material-icons"></img></h2>
+                      <h2 className="center"><img src="/images/boxicon.png" className="material-icons"></img></h2>
                       <h5 className="center">Speeds up development</h5>
                       <p className="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
                     </div>
@@ -48,7 +55,7 @@ export default class About extends PureComponent {
 
                   <div className="col s12 m4">
                     <div className="icon-block">
-                      <h2 className="center light-blue-text"><img src="/images/iconHeart.png" className="material-icons"></img></h2>
+                      <h2 className="center light-blue-text"><img src="/images/scanicon.png" className="material-icons"></img></h2>
                       <h5 className="center">User Experience Focused</h5>
                       <p className="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
                     </div>
@@ -56,7 +63,7 @@ export default class About extends PureComponent {
 
                   <div className="col s12 m4">
                     <div className="icon-block">
-                      <h2 className="center light-blue-text"><img src="/images/iconBat.png" className="material-icons"></img></h2>
+                      <h2 className="center light-blue-text"><img src="/images/papericon.png" className="material-icons"></img></h2>
                       <h5 className="center">Easy to work with</h5>
                       <p className="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
                     </div>
@@ -67,7 +74,7 @@ export default class About extends PureComponent {
             </div>
 
             {/* Footer */}
-            <footer className="page-footer white">
+            <footer className="page-footer">
               <div className="container">
                 <div className="row">
                   <div className="col l6 s12">
